cfa13982cde03bd470b9ef4d6dcb67bd1f3ed4b6
Jonathan Smith <jonathan@impacti.org>
2010-12-16 23:43:22 +0000

    Now it does select only the unique projects per staff!

--- app/controllers/pipelines_controller.rb
+++ app/controllers/pipelines_controller.rb
@@ -1,14 +1,6 @@
 class PipelinesController < ApplicationController
   def show
     @leader = Project.find(params[:id]).pipelines.last.lead
-    all_pipelines = Pipeline.find_all_by_lead(@leader)
-    @pipelines = []
-    all_pipelines.each do |check|
-      if @pipelines.include?(check)
-        then
-      else
-        @pipelines << check
-      end
-    end 
+    @pipelines = Pipeline.find_all_by_lead(@leader, :select => 'DISTINCT project_id')
   end
 end
