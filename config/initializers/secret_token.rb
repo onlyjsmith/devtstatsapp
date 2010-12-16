3f41ae9241f5fc39297c0a83c577c86707c482b9
Jonathan Smith <jonathan@impacti.org>
2010-12-16 15:32:21 +0000

    First commit - empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devtstatsapp::Application.config.secret_token = '70a7ea9c17c32ccb6241040a1c3f96048fa2215712689f434f665c45c7d1bd117c46c60fa3c2cbc332d15766ede4e96c1847bc3023855bc99ca69b4f5d86ac56'
